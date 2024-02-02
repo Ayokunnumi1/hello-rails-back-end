dd2e7e9f0e239c1dd46b1ec2915d2afa0ad79610
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-02 22:15:04 +0100

    Update CORS

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module HelloRailsBackEnd
     
     config.middleware.insert_before 0, Rack::Cors do
         allow do
-          origins 'localhost:5173'
+          origins '*'
           resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
         end
       end
