043dad42dfa3ee0ea05cfdf599d1dc4e6386c359
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-02 21:47:33 +0100

    Update CORS

--- config/application.rb
+++ config/application.rb
@@ -38,13 +38,14 @@ module HelloRailsBackEnd
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins 'localhost:5173'
+          resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+        end
+      end
   end
 
-  config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-      end
-    end
 
 end
