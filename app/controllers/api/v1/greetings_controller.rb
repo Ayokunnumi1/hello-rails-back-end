174cd715dccf3ab933b6f72bf246725e33acab2c
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-02 22:43:24 +0100

    Fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
-    def random_greeting
+  def random_greeting
     random_greeting = Greeting.order('RANDOM()').first
     formatted_greeting = { message: random_greeting&.message }
     render json: JSON.pretty_generate(formatted_greeting)
